11d247a0810febf1ff26e7836562fbc36f8a4f32
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-31 21:07:31 +0530

    bendning the event handlers using constructor and base constructor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,40 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: []
+        tags: ["tag1", "tag2", "tag3"]
     };
- 
- renderTags(){
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
- }
- 
-    render() {
+
+constructor (){
+    super (); //base constructor
+this.handleIncrement = this.handleIncrement.bind(this);
+}
+
+handleIncrement(){
+    console.log('Increment Clicked');
+}
+
+    render() { 
         return (
         <div>
-        {this.state.tags.length === 0} <p>This is a another method to render a message</p>
-        {this.state.tags.length === 0 && "Here we are using the logical && operator to render a message using boolean && string concept"}
-        {this.renderTags()}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick = {this.handleIncrement} 
+            className="badge badge-secondary badge-sm">
+                Increment</button>
+            <ul>{this.state.tags.map(tag => <li key = {tag.id}>{tag}</li>)}</ul>
         </div>
         );
     }
-} 
+
+getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+return classes;
+}
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
 export default Counter;
