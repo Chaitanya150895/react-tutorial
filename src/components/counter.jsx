a9ea083c8f7d2039924cd4bf205ba23ccecad68d
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-31 17:02:27 +0530

    Rendering Classes Dynamically, changing the class badge to warning when value is zero else badge class is primary

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 10,
+        imageUrl: "https://picsum.photos/id/1084/536/354?grayscale"
+    };
+
+    style = {
+        fontSize : 20,
+        fontWeight : "bold"
     };
 
     render() { 
         return (
         <div>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="badge badge-secondary badge-sm">Increment</button>
         </div>
         );
     }
+
+getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+return classes;
+}
 
     formatCount(){
         const {count} = this.state;
